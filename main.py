ac603b7866a0e292573927c99471c04fce532c1f
viv-codes <vhafener@outlook.com>
2022-02-27 21:20:13 -0500

    More work and ran formatter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from black import out
 from prompt_toolkit.shortcuts import input_dialog, message_dialog, button_dialog
+
 
 def main():
     # Elements
@@ -15,36 +15,63 @@ def main():
     # Logic Flow - later on make it so that I could also just call directly from the command line
 
     instr = input()
+    verify(instr)
     outstr = output()
+    verify(outstr)
     nameout = outname()
+    verify(nameout)
+    confirm(instr, outstr, nameout)
 
 
 def input():
-    instr = input_dialog(title='autosort CLI', text='Path of input:').run()
+    instr = input_dialog(title="autosort CLI", text="Path of input:").run()
     if instr == "":
         input()
     else:
         return instr
 
+
 def output():
-    outstr = input_dialog(title='autosort CLI', text='Path of output:').run()
+    outstr = input_dialog(title="autosort CLI", text="Path of output:").run()
     if outstr == "":
         output()
     else:
         return outstr
 
+
 def outname():
-    name = input_dialog(title='autosort CLI', text='Name of output folder:').run()
+    name = input_dialog(title="autosort CLI", text="Name of output folder:").run()
     if name == "":
         outname()
     else:
         return name
 
+
 def confirm(instr, outstr, nameout):
-    return button_dialog(title='autosort CLI',text='Confirm:\nInput folder:'+instr+'\nOutput folder:'+outstr+'\nName of output folder:'+nameout+'\nIs this information correct?', buttons=[('Yes', True),('No', False),('Cancel',None)])
+    out = button_dialog(
+        title="autosort CLI",
+        text="Confirm:\nInput folder:"
+        + instr
+        + "\nOutput folder:"
+        + outstr
+        + "\nName of output folder:"
+        + instr
+        + "/"
+        + nameout
+        + "\nIs this information correct?",
+        buttons=[("Yes", True), ("No", False), ("Cancel", None)],
+    ).run()
+    return out
+
+
+def verify(checkstr):
+    if checkstr == None:
+        exit()
+
 
 def genericmessage(astring):
-    message_dialog(title='autosort CLI', text=astring+'\nPress ENTER to quit.').run()
+    message_dialog(title="autosort CLI", text=astring + "\nPress ENTER to quit.").run()
+
 
 if __name__ == "__main__":
     main()
