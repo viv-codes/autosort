9a78d60d757d206657f6e63eb5c7b198d30fe592
viv-codes <vhafener@outlook.com>
2022-02-28 19:19:42 -0500

    New release: v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="filesort",
-    version="0.1.0",
+    version="0.1.1",
     author="Vivian Hafener",
     author_email="vhafener@outlook.com",
     description="A package that aids sorting of files",
